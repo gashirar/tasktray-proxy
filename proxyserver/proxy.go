a8885e47922118ec6512b041d19cc12ee902dbed
t-sawagashira <t-sawagashira>
2018-03-26 14:09:37 +0900

    remove proxy library

--- proxyserver/proxy.go
+++ proxyserver/proxy.go
@@ -10,8 +10,6 @@ import (
 	"net"
 	"io"
 	"encoding/base64"
-
-	"github.com/elazarl/goproxy"
 )
 
 type IProxy interface {
@@ -49,6 +47,50 @@ func (p *Proxy) Shutdown() (err error) {
 	err = p.server.Shutdown(ctx)
 	return
 }
+
+func (p *Proxy) handleHttps(w http.ResponseWriter, r *http.Request) {
+	dest_conn, err := net.DialTimeout("tcp", r.Host, 10*time.Second)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	hijacker, ok := w.(http.Hijacker)
+	if !ok {
+		http.Error(w, "Hijacking not supported", http.StatusInternalServerError)
+		return
+	}
+	client_conn, _, err := hijacker.Hijack()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+	}
+	go transfer(dest_conn, client_conn)
+	go transfer(client_conn, dest_conn)
+}
+
+func (p *Proxy) handleHttp(w http.ResponseWriter, r *http.Request) {
+	hj, _ := w.(http.Hijacker)
+	client := &http.Client{}
+	r.RequestURI = ""
+	if resp, err := client.Do(r); err != nil {
+		log.Print(err)
+	} else if conn, _, err := hj.Hijack(); err != nil {
+		log.Print(err)
+	} else {
+		defer conn.Close()
+		resp.Write(conn)
+	}
+}
+
+func (p *Proxy) handleRequest(w http.ResponseWriter, r *http.Request) {
+	log.Printf("[%s] %s", r.Method, r.URL)
+	if r.Method == "CONNECT" {
+		p.handleHttps(w, r)
+	} else {
+		p.handleHttp(w, r)
+	}
+}
+
 
 type AuthProxy struct {
 	*Proxy
@@ -129,7 +171,7 @@ func NewProxy(host string, port string) *Proxy {
 		ExpectContinueTimeout: 1 * time.Second,
 	}
 
-	p.handler = goproxy.NewProxyHttpServer()
+	p.handler = http.HandlerFunc(p.handleRequest)
 
 	return p
 }
