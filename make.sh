7fe40263000f61b8f022cc9cbdc1b44f785ab559
gashirar <spin32632@gmail.com>
2019-02-03 07:06:33 -0800

    Add build script

---
+++ make.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ $# != 1 ]; then
+	echo "Usage: $0 [binary name]"
+	exit 0
+fi
+
+GOOS=linux    GOARCH=amd64 go build -o ./bin/$1
+GOOS=windows  GOARCH=amd64 go build -o ./bin/$1.exe
+# GOOS=darwin GOARCH=amd64 go build -o ./bin/darwin64/$1
